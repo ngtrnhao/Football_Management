169cee6308665adbfa92b934522a03d322b18966
Nguyen Truong Nhat Hao <nguyentruongnhathao1922@gmail.com>
2024-12-11 20:48:21 +0700

    <sd>

---
+++ Soccer/Scripts/script.js
@@ -0,0 +1,8 @@
+﻿// Simple animation for CTA button on hover
+document.querySelector('.cta-button').addEventListener('mouseover', function () {
+    this.style.transform = 'scale(1.1)';
+});
+
+document.querySelector('.cta-button').addEventListener('mouseout', function () {
+    this.style.transform = 'scale(1)';
+});
